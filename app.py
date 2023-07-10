ab9c0f179ef16124516c878a903e34faef9bb3fa
Dmitry Golubchikov <dmitry.golubnikov@yandex.ru>
2023-07-10 16:33:11 +0500

    добавлена функция отрисовки гистограмм

--- app.py
+++ app.py
@@ -10,119 +10,93 @@ import plotly.io as poi
 
 poi.renderers.default = 'browser'
 
-df = pd.read_excel('date_2023_year.xlsx')
-df['Код вида расходов'] = df['Код вида расходов'].astype(str)
+dict_conv={
+    'Глава': lambda x: str(x),
+    'Код вида расходов': lambda x: str(x)
+    }
+
+df = pd.read_excel('date_2023_year.xlsx', converters=dict_conv)
 
 app = dash.Dash(__name__, external_stylesheets=[dbs.themes.BOOTSTRAP])
 
-fig_pie = px.pie(data_frame=df, values='Доведено ЛБО', names='Код вида расходов', title='Распределение ЛБО по видам расходов')
+fig_pie = px.pie(data_frame=df, values='Доведено ЛБО', names='Код вида расходов', title='Распределение ЛБО по видам расходов',)
 
-fig_histograms_types_of_expenses = go.Figure()
-fig_histograms_types_of_expenses.add_trace(go.Histogram(
-    x=df['Код вида расходов'],
-    y=df['Доведено ЛБО'],
-    #  xbins=dict(
-    #     start=100.0,
-    #     end=900.0,
-    #     size=50.0),
-    name='Доведено ЛБО',
-    histfunc = 'sum',
-    marker_color='#EB89B5',
-    opacity=0.75,
-))
+def create_hisogram(x_value:str, y1_value:str,
+                    y2_value:str, y3_value:str,
+                    title_text:str, xaxis_title_text:str,
+                    yaxis_title_text:str):
+    fig_histograms = go.Figure()
+    fig_histograms.add_trace(go.Histogram(
+        x=df[x_value],
+        y=df[y1_value],
+        name=y1_value,
+        histfunc = 'sum',
+        marker_color='#EB89B5',
+        opacity=0.75,
+    ))
 
-fig_histograms_types_of_expenses.add_trace(go.Histogram(
-    x=df['Код вида расходов'],
-    y=df['Принято БО: всего'],
-    name='Принято БО: всего',
-    histfunc = 'sum',
-    # xbins=dict(
-    #     start=100.0,
-    #     end=900.0,
-    #     size=50.0),
-    marker_color='#330C73',
-    opacity=0.75
-))
+    fig_histograms.add_trace(go.Histogram(
+        x=df[x_value],
+        y=df[y2_value],
+        name=y2_value,
+        histfunc = 'sum',
+        marker_color='#330C73',
+        opacity=0.75
+    ))
 
-fig_histograms_types_of_expenses.add_trace(go.Histogram(
-    x=df['Код вида расходов'],
-    y=df['Исполнено ДО'],
-    name='Исполнено ДО',
-    histfunc = 'sum',
-    #  xbins=dict(
-    #     start=100.0,
-    #     end=900.0,
-    #     size=50.0),
-    marker_color='#FF1493',
-    opacity=0.75
-))
+    fig_histograms.add_trace(go.Histogram(
+        x=df[x_value],
+        y=df[y3_value],
+        name=y3_value,
+        histfunc = 'sum',
+        marker_color='#FF1493',
+        opacity=0.75
+    ))
 
-fig_histograms_types_of_expenses.update_layout(
-    title_text='Виды расходов',
-    xaxis_title_text='Вид расходов',
-    yaxis_title_text='Сумма'
-    # bargap=0.2,
-    # bargroupgap=0.1
-)
+    fig_histograms.update_layout(
+        title_text=title_text,
+        xaxis_title_text=xaxis_title_text,
+        yaxis_title_text=yaxis_title_text
+    )
+    return fig_histograms
 
-fig_histograms_national_project = go.Figure()
-fig_histograms_national_project.add_trace(go.Histogram(
-    x=df['Код НП'],
-    y=df['Доведено ЛБО'],
-    name='Доведено ЛБО',
-    histfunc='sum',
-    marker_color='#EB89B5',
-    opacity=0.75
-))
-
-fig_histograms_national_project.add_trace(go.Histogram(
-    x=df['Код НП'],
-    y=df['Принято БО: всего'],
-    name='Принято БО: всего',
-    histfunc='sum',
-    marker_color='#330C73',
-    opacity=0.75
-))
-
-fig_histograms_national_project.add_trace(go.Histogram(
-    x=df['Код НП'],
-    y=df['Исполнено ДО'],
-    name='Исполнено ДО',
-    histfunc='sum',
-    marker_color='#FF1493',
-    opacity=0.75
-))
-
-fig_histograms_national_project.update_layout(
-    title_text='Национальные проекты',
-    xaxis_title_text='Код национального проекта',
-    yaxis_title_text='Сумма'
-)
 app.layout = html.Div([
     dbs.Row(html.H1('Отчёт по исполнению Федерального бюджета по расходам УФК по Пермскому краю')),
     dbs.Row([
         dbs.Col([
-            # html.Div('Виды расходов'),
             dcc.Graph(figure=fig_pie)
         ],
-        width={'size':4}
+        width={'size':6}
         ),
         dbs.Col([
-            # html.Div('Уровень принятых БО к ЛБО'),
-            dcc.Graph(figure=fig_histograms_types_of_expenses)
+            dcc.Graph(figure=create_hisogram('Код вида расходов', 'Доведено ЛБО', 'Принято БО: всего',
+                                             'Исполнено ДО', 'Распределение по видам расходов', 'Виды расходов', 'Сумма'))
         ],
-        width={'size':4}
+        width={'size':6}
+        )
+    ],
+    justify='start'
+),
+    dbs.Row([
+        dbs.Col([
+            dcc.Graph(figure=create_hisogram('Код НП', 'Доведено ЛБО', 'Принято БО: всего',
+                                             'Исполнено ДО', 'Распределение по национальным проектам', 'Национальный проект', 'Сумма'))
+        ],
+        width={'size':6}
         ),
         dbs.Col([
-            dcc.Graph(figure=fig_histograms_national_project)
-        ], width={'size':4}
+            dcc.Graph(figure=create_hisogram('Глава', 'Доведено ЛБО', 'Принято БО: всего',
+                                             'Исполнено ДО', 'Распределение по главам', 'Глава', 'Сумма'))
+        ],
+        width={'size':6}
         )
     ],
     justify='start'
 )
-],style={'text-align':'left',
+    ],
+style={'text-align':'left',
          'margin-left':'20px'}
-         )
+)
 
 if __name__ == '__main__':
     app.run_server(debug=True)
